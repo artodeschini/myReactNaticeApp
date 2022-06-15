16264ac696bfd0aa9d7f3f977a0da88328d81298
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-06-15 11:17:25 -0300

    trabalhando com state que são variaveis

--- App.js
+++ App.js
@@ -2,43 +2,24 @@ import React, { Component } from "react";
 import { View, Text, Image } from 'react-native'
 
 
-function App() {
+class App extends Component {
 
-  let nome = 'Artur Todeschini Crestani';
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Artur'
+    };
+  }
 
-  return(
-    <View>
-      <Text>Ola Mundo</Text>
-      <Text>Meu primeiro App</Text>
-      <Text style={{ color: 'red', fontSize: 25, margin: 15 }} >
-        Texto em vernelho</Text>
-
-      <Jobs largura={500} altura={200} nome={'Artur'}/>
-      <Jobs largura={500} altura={200} fulano={'Jobs'}/>
-      <Text style={{ fontSize:  30}}>{nome}</Text>
-    </View>
-  )
-}
-
-export default App;
-
-
-// primeiro exemplo com componete
-
-class Jobs extends Component {
-  render() {
-
-    let img = 'https://sujeitoprogramador.com/steve.png';
-
+  render(){
     return(
-      <View>
-        <Image
-            source={{ uri: img}}
-            //style={{ width: 300, height: 300 }}>
-            style={{ width: this.props.largura, height: this.props.altura }}>
-          </Image>
-          <Text>{this.props.fulano}</Text>
+      <View style={{ marginTop: 20 }} >
+        <Text style={{ fontSize: 23, color: 'red', textAlign: 'center'}}>
+          {this.state.name}</Text>
       </View>
     );
   }
+
 }
+
+export default App;
