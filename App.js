220268fb088536a13d4c01e5166f58715e4807db
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-06-15 11:28:32 -0300

    primeiro botao com acao

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, Image } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 
 class App extends Component {
@@ -7,15 +7,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Artur'
+      nome: ''
     };
+
+    this.entrar = this.entrar.bind(this);
+  }
+
+  entrar(argumento) {
+    this.setState({
+      nome: argumento
+    })
   }
 
   render(){
     return(
       <View style={{ marginTop: 20 }} >
+        <Button title="Entrar" onPress={() => this.entrar('Artur Todeschini Crestani')}></Button>
         <Text style={{ fontSize: 23, color: 'red', textAlign: 'center'}}>
-          {this.state.name}</Text>
+          {this.state.nome}</Text>
       </View>
     );
   }
