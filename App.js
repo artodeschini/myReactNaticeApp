a724d22840fb6a306247880ee2de47b643daa70a
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-06-14 23:29:07 -0300

    adicionado style e images ao primeiro app

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 
 function App() {
   return(
     <View>
       <Text>Ola Mundo</Text>
+      <Text>Meu primeiro App</Text>
+      <Text style={{ color: 'red', fontSize: 25, margin: 15 }} >
+        Artur Todeschini Crestani</Text>
+
+      <Image 
+         // obrigatorio source e style com width and height 
+        source={{ uri: 'https://sujeitoprogramador.com/steve.png'}}
+        style={{ width: 300, height: 300 }}
+        />
     </View>
   )
 }
