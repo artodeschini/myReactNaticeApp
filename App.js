6e0f83a62411c45845b65dc4da83e0bb84612ac2
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-06-14 23:49:20 -0300

    criando meu primeiro componente

--- App.js
+++ App.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, Text, Image } from 'react-native'
 
 
 function App() {
 
   let nome = 'Artur Todeschini Crestani';
-  let img = 'https://sujeitoprogramador.com/steve.png';
-
 
   return(
     <View>
       <Text>Ola Mundo</Text>
       <Text>Meu primeiro App</Text>
       <Text style={{ color: 'red', fontSize: 25, margin: 15 }} >
-        Artur Todeschini Crestani</Text>
+        Texto em vernelho</Text>
 
-      <Image 
-         // obrigatorio source e style com width and height 
-        source={{ uri: img }}
-        style={{ width: 300, height: 300 }}
-        />
+      <Jobs largura={500} altura={200} nome={'Artur'}/>
+      <Jobs largura={500} altura={200} fulano={'Jobs'}/>
       <Text style={{ fontSize:  30}}>{nome}</Text>
     </View>
   )
 }
 
 export default App;
+
+
+// primeiro exemplo com componete
+
+class Jobs extends Component {
+  render() {
+
+    let img = 'https://sujeitoprogramador.com/steve.png';
+
+    return(
+      <View>
+        <Image
+            source={{ uri: img}}
+            //style={{ width: 300, height: 300 }}>
+            style={{ width: this.props.largura, height: this.props.altura }}>
+          </Image>
+          <Text>{this.props.fulano}</Text>
+      </View>
+    );
+  }
+}
