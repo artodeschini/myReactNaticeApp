a3e63474cd970ec79521acf5939393c261ff7858
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-06-14 22:46:56 -0300

    Ola mundo

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from 'react-native'
+
+
+function App() {
+  return(
+    <View>
+      <Text>Ola Mundo</Text>
+    </View>
+  )
+}
+
+export default App;
