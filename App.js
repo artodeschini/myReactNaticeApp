0648032d742d779c4e935589c5ac565c3ff4e622
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-06-15 13:31:43 -0300

    trabalhando com flex proporcao de telas

--- App.js
+++ App.js
@@ -1,54 +1,30 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
 
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      nome: ''
-    };
-
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar(argumento) {
-    this.setState({
-      nome: argumento
-    })
-  }
-
   render(){
     return(
-      <View style={ styles.area } >
-
-        <Text style={ [styles.textoPrincipal, styles.alinhaTextoCentro] }>Exemplo texto 1</Text>
-        <Text style={ styles.alinhaTextoCentro}>Exemplo texto 2</Text>
-        <Text>Exemplo texto 3</Text>
-        <Text>Exemplo texto 4</Text>
-        <Text>Exemplo texto 5</Text>
-        <Text style={ styles.textoPrincipal}>Exemplo texto 6</Text>
+      // flex 1 diz que para o componete usar a maior area possivel
+      // flex 2 diz que ocupara 50 % e as outras duas 25%
+      <View style={{ flex:1, backgroundColor: '#222'}}>
+        <View style={{
+          flex: 1,
+          backgroundColor: 'red'
+        }}></View>
+         <View style={{
+          flex: 1,
+          backgroundColor: 'green'
+        }}></View>
+        <View style={{
+          flex: 2,
+          backgroundColor: 'yellow'
+        }}></View>
       </View>
     );
   }
 
 }
-
-const styles = StyleSheet.create({
-  area: {
-    marginTop: 40
-  },
-
-  textoPrincipal: {
-    fontSize: 25,
-    color: '#FF0000'
-  },
-
-  alinhaTextoCentro: {
-    textAlign: 'center'
-  }
-
-});
 
 export default App;
