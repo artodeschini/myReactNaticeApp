f86a3fa3b8afd07f0a78fc1ae63fb5ebe55835e4
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-06-15 13:53:20 -0300

    alinhamentos flex com flexDirection alignItens e justifyContent

--- App.js
+++ App.js
@@ -6,8 +6,21 @@ class App extends Component {
 
   render(){
     return(
-      //comportamento padrao box um em baixo do outro
-      <View style={{ flex:1}}>
+      // comportamento padrao box um em baixo do outro ou flexDirection column
+      // utilziando row os compoentes ficarao um ao lado do outro
+      // usando justifyContent posso alinhar ao centro 
+      // ou flex-end a direita e flex-start a esquerda
+      // ou ainda space-between espacos iguais entre deles
+      // ou ainda space-around espacos iguais ao redores deles
+
+      // alignItems posso colocar no centro ou no comeco com flex-start ou no final com flex-end 
+
+      <View style={{ 
+        flex:1,
+        flexDirection: 'row', // row , column
+        justifyContent: 'center', // center, flex-start, flex-end
+        alignItems: 'center' // center, flex-start, flex-end
+        }}>
         <View style={{
           height: 50,
           width: 50,
