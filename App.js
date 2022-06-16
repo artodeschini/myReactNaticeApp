54f89b172bc96121f33c4dd72b1bf278c88ad067
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-06-15 22:28:13 -0300

    adicionado slider ao app

--- App.js
+++ App.js
@@ -1,62 +1,30 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from 'react-native';
-// import {Picker} from '@react-native-picker/picker';
-// versoes antigas pode usar
-// https://www.npmjs.com/package/@react-native-community/picker
+import Slider from "@react-native-community/slider";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
-      pizza: 0,
-      pizzas: [
-         { key: '1', nome: 'Calabresa', valor: 35.90, },
-         { key: '2', nome: '4 Queijos', valor: 39.90, },
-         { key: '3', nome: 'Vegetariana', valor: 40.20, },
-         { key: '4', nome: 'File 4 Queijos', valor: 29.90, },
-         { key: '5', nome: 'Calabresa', valor: 32.90, },
-         { key: '6', nome: 'Portuguesa', valor: 33.90, },
-      ]
+      valor: 0
     }
   }
 
   render(){
- 
-    let pizzaItens = this.state.pizzas.map(
-      (v, k) => {
-        return <Picker.Item key={k} value={k} label={v.nome}></Picker.Item>
-      }
-    ); 
-
     return(
       <View style={styles.container}>
-        <Text style={styles.logo}>Menu Pizza</Text>
-
-        <Picker 
-          selectedValue={this.state.pizza}
-          onValueChange={ (itemValue, itemIndex) => 
-            this.setState({
-              pizza: itemIndex
-            })
-          }>
-          {/*
-            valores fixos seria assim 
-          <Picker.Item key={0} value={0} label={"Calabresa"}></Picker.Item>
-          <Picker.Item key={1} value={1} label={"4 Queijos"}></Picker.Item>
-          <Picker.Item key={2} value={2} label={"Vegetariana"}></Picker.Item>
-          <Picker.Item key={3} value={3} label={"Gorgonzola"}></Picker.Item>
-          <Picker.Item key={4} value={4} label={"File 4 Queijos"}></Picker.Item>
-          <Picker.Item key={5} value={5} label={"Portuguesa"}></Picker.Item> 
-          
-          */}
-          {pizzaItens}
-        </Picker>
-
-        <Text style={styles.pizzas}>Você escolheu Pizza {this.state.pizzas[this.state.pizza].nome}</Text>
-        <Text style={styles.pizzas}>R$ {this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
-        {/* <Text style={styles.pizzas}>{this.state.pizza}</Text> */}
+        <Slider
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={(valorSelecionado) => this.setState({valor: valorSelecionado})}
+          value={this.state.valor}
+          minimumTrackTintColor='green'
+          maximumTrackTintColor="red"
+        />
+        <Text style={{ textAlign: 'center', fontSize: 30}}>
+          {this.state.valor.toFixed(1)}
+        </Text>
       </View>
     );
   }
@@ -68,19 +36,7 @@ const styles = StyleSheet.create({
   
   container: {
     flex: 1,
-    marginTop: 20
-  }, 
-
-  logo: {
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: "bold"
-  },
-
-  pizzas: {
-    marginTop: 15,
-    fontSize: 25,
-    textAlign: "center"
+    marginTop: 15
   }
 });
 
