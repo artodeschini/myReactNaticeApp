47d64ddf1bec48fd1881494f2848411437955eac
Artur Todeschini Crestani <artodeschini@gmail.com>
2022-07-02 20:07:01 -0300

    visual quase ok do clone

--- App.js
+++ App.js
@@ -10,51 +10,51 @@ class App extends Component {
     super(props);
     this.state = {
       feed: [
-        // {
-        //   id: '1', 
-        //   nome: 'Lucas Silva', 
-        //   descricao: 'Mais um dia de muitos bugs :)', 
-        //   imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
-        //   imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
-        //   likeada: false, 
-        //   likers: 0
-        //  },
-        // {
-        //   id: '2', 
-        //   nome: 'Matheus', 
-        //   descricao: 'Isso sim é ser raiz!!!!!', 
-        //   imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
-        //   imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
-        //   likeada: false, 
-        //   likers: 0
-        // },
-        // {
-        //   id: '3', 
-        //   nome: 'Jose Augusto', 
-        //   descricao: 'Bora trabalhar Haha', 
-        //   imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
-        //   imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
-        //   likeada: false, 
-        //   likers: 3
-        // },
-        // {
-        //   id: '4', 
-        //   nome: 'Gustavo Henrique', 
-        //   descricao: 'Isso sim que é TI!', 
-        //   imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
-        //   imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
-        //   likeada: false, 
-        //   likers: 1
-        // },
-        // {
-        //   id: '5', 
-        //   nome: 'Guilherme', 
-        //   descricao: 'Boa tarde galera do insta...', 
-        //   imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
-        //   imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
-        //   likeada: false, 
-        //   likers: 32
-        // }
+        {
+          id: '1', 
+          nome: 'Lucas Silva', 
+          descricao: 'Mais um dia de muitos bugs :)', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
+          likeada: false, 
+          likers: 0
+         },
+        {
+          id: '2', 
+          nome: 'Matheus', 
+          descricao: 'Isso sim é ser raiz!!!!!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
+          likeada: false, 
+          likers: 0
+        },
+        {
+          id: '3', 
+          nome: 'Jose Augusto', 
+          descricao: 'Bora trabalhar Haha', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
+          likeada: false, 
+          likers: 3
+        },
+        {
+          id: '4', 
+          nome: 'Gustavo Henrique', 
+          descricao: 'Isso sim que é TI!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
+          likeada: false, 
+          likers: 1
+        },
+        {
+          id: '5', 
+          nome: 'Guilherme', 
+          descricao: 'Boa tarde galera do insta...', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+          likeada: false, 
+          likers: 32
+        }
       ]
      };
   }
@@ -78,11 +78,12 @@ class App extends Component {
           </TouchableOpacity>
         </View>
 
-        {/* <FlatList
-          showsHorizontalScrollIndicator={false}
-          data={this.state.feed}
-          renderItem={({item}) => <Lista data={item}/>} >
-        </FlatList> */}
+        <FlatList
+          showsHorizontalScrollIndicator={false} // faz nao aparecer a barra de reolagem
+          keyExtractor={(item) => item.id }
+          data={this.state.feed} // dados da lista
+          renderItem={({item}) => <Lista data={item}/>}>
+        </FlatList>
       </View>
     );
   }
--- src/lista/index.js
+++ src/lista/index.js
@@ -1,24 +1,179 @@
 import React, {Component} from "react";
-import {View, Text, Image, TouchableOpacity} from "react-native";
+import {View, Text, Image, TouchableOpacity, StyleSheet} from "react-native";
 
 class Lista extends Component {
 
     constructor(props) {
         super(props);
 
-        // this.state = {
-        //     feed: this.props.data
-        // };
+        this.state = {
+            feed: this.props.data
+        };
+
+        this.mostraLikes = this.mostraLikes.bind(this);
+        this.like = this.like.bind(this);
+        this.carregarIconeLike = this.carregarIconeLike.bind(this); 
     }
 
+    carregarIconeLike(likeada) {
+        return likeada ? require('../img/likeada.png') : require('../img/like.png');
+    }
+
+    mostraLikes(likers) {
+        let feed = this.state.feed;
+
+        if (feed.likers) {;
+            return(
+                <Text style={styles.likers}>
+                    {feed.likers} {feed.likers > 1 ? 'curtidas' : 'curtida'}
+                </Text>     
+            );
+        } else {
+            return;
+        }
+    }
+
+    like() {
+        let feed = this.state.feed;
+
+        if (feed.likeada) {
+            this.setState({
+                feed: {
+                    ...feed, // d+ propriedade para nao sobrescrever
+                    likeada: false,
+                    likers: feed.likers -1
+                }
+            })
+        } else {
+            this.setState({
+                feed: {
+                    ...feed,
+                    likeada: true,
+                    likers: feed.likers +1
+                }
+            })
+        }
+
+    }
 
     render() {
         return(
-            <View>
-                <Text>Feed...</Text>
+            <View style={styles.areaFeed}>
+                <View style={styles.viewPerfil}>
+                    <Image source={{uri: this.state.feed.imgperfil}}
+                        style={styles.fotoPerfil}>
+                    </Image>
+                    <Text style={styles.nomeUsuario}>
+                        {this.state.feed.nome}
+                    </Text>
+                </View>
+
+                <Image resizeMode="cover"
+                    style={styles.fotoPublicacao}
+                    source={{uri:  this.state.feed.imgPublicacao}}>
+
+                </Image>
+                <View style={styles.areaBtn}>
+                    <TouchableOpacity onPress={this.like}>
+                        {/* <Image source={require('../img/like.png')} */}
+                        <Image source={this.carregarIconeLike(this.state.feed.likeada)}
+                                style={styles.iconelike}>   
+                        </Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={require('../img/send.png')}
+                            style={styles.iconelike}>
+                                
+                        </Image>
+                    </TouchableOpacity>
+                </View>
+
+                {this.mostraLikes(this.state.feed.likers)}
+
+                <View style={styles.viewRodape}>
+                    <Text style={styles.nomeRodape}>
+                        {this.state.feed.nome}
+                    </Text>
+                    <Text style={styles.descRodape}>
+                        {this.state.feed.descricao}
+                    </Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    areaFeed: {
+
+    },
+
+    nomeUsuario: {
+        fontSize: 22,
+        textAlign: 'left',
+        color: '#000000',
+        padding: 5
+    },
+
+    fotoPerfil: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+
+    },
+
+    fotoPublicacao: {
+        flex: 1,
+        height: 400,
+        alignItems: 'center'
+    },
+
+    viewPerfil: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+        padding: 8,
+
+    },
+
+    areaBtn: {
+        flexDirection: 'row',
+        padding: 5
+
+    },
+
+    iconelike: {
+        width: 33,
+        height: 33
+    },
+
+    btnSend: {
+        paddingLeft: 5
+    },
+
+    viewRodape: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+
+    descRodape: {
+        paddingLeft: 5,
+        fontSize: 15,
+        color: '#000'
+    },
+
+    nomeRodape: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: '#000',
+        paddingLeft: 5
+
+    },
+
+    likers: {
+        fontWeight: "bold",
+        marginLeft: 5,
+    }
+});
 
 export default Lista;
